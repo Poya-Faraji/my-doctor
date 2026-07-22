78f755b3cb3b935eb5f5388207482e12897e358c
Poya Faraji <poya.faraji@outlook.com>
2026-07-22 20:23:45 +0330

    [ADD] styles to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+import { ReactNode } from "react";
+
 import GlobalSearchBoxComponent from "@/components/global-search-box/global-search-box.component";
+
+import MyDoctorLogo from "@/logo/my-doctor";
+
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Home(): ReactNode {
   return (
-    <div className={styles.page}>
+    <div className={styles.home}>
+      <h1>
+        <MyDoctorLogo />
+        My doctor
+      </h1>
       <GlobalSearchBoxComponent />
+      <div className={styles.history}>
+        <div className={styles.title}>Your recent searches</div>
+        <ul>
+          <li>Orthopedic</li>
+          <li>Cardiovascular</li>
+        </ul>
+      </div>
     </div>
   );
 }
