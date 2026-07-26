94992f027967eccab8f76c19eaa910014f0b6400
Poya Faraji <poya.faraji@outlook.com>
2026-07-26 20:05:59 +0330

    [ADD] not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import { ReactElement } from "react";
+import Image from "next/image";
+
+import notFoundImage from "@/assets/illustrations/not-found.svg";
+
+import GlobalSearchBoxComponent from "@/components/global-search-box/global-search-box.component";
+
+import styles from "./not-found.module.css";
+
+export default function NotFound(): ReactElement {
+  return (
+    <div className={styles["not-found"]}>
+      <div className={styles.writings}>
+        <div className={styles["status-code"]}>Error 404</div>
+        <h1>Page you are looking is not found</h1>
+        <p>
+          Please search for your desired doctor or treatment center using
+          the search box.
+        </p>
+      </div>
+      <div className={styles.visuals}>
+        <Image src={notFoundImage} alt="" />
+      </div>
+      <div className={styles.search}>
+        <GlobalSearchBoxComponent />
+      </div>
+    </div>
+  );
+}
